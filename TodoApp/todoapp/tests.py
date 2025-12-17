816e6b18c91e3c91490327c54f970b63d2fe5a37
KW <k.wojtasinski@wp.pl>
2025-12-17 23:06:45 +0100

    [TodoApp] UnitTesting

--- TodoApp/todoapp/tests.py
+++ TodoApp/todoapp/tests.py
@@ -1,3 +1,45 @@
 from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
 
-# Create your tests here.
+from .models import Todo
+
+
+class TodoAppTests(TestCase):
+
+    def test_home_view(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_add_todo_post(self):
+        response = self.client.post(
+            reverse("add_todo"),
+            {"content": "Test item"},
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Todo.objects.count(), 1)
+
+    def test_add_todo_get_redirect(self):
+        response = self.client.get(reverse("add_todo"))
+        self.assertEqual(response.status_code, 302)
+
+    def test_delete_existing_todo(self):
+        todo = Todo.objects.create(
+            content="To delete",
+            add_date=timezone.now()
+        )
+
+        response = self.client.post(
+            reverse("delete_todo", args=[todo.id])
+        )
+
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(Todo.objects.count(), 0)
+
+    def test_delete_non_existing_todo(self):
+        response = self.client.post(
+            reverse("delete_todo", args=[999])
+        )
+
+        self.assertEqual(response.status_code, 302)

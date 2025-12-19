7c5b252de336a4160cadf19e1c9fd7d706e1ce93
KW <k.wojtasinski@wp.pl>
2025-12-19 22:24:37 +0100

    [Project_Django] upgrade dependencies to Django 5+

--- Project_Django/WebSite_Django/WebSite_Django/settings.py
+++ Project_Django/WebSite_Django/WebSite_Django/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
-#import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -131,5 +130,3 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-#django_heroku.settings(locals())

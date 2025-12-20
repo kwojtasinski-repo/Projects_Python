26c7b9d025160d8f7e8bc9d877fb574432b8819a
KW <k.wojtasinski@wp.pl>
2025-12-21 00:16:50 +0100

    [mysite] urls

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -6,14 +6,5 @@ urlpatterns=[
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
-    #path('', views.index, name='index'),
-    # ex: /polls/5/
-    #path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    #path('<int:question_id>/results/', views.results, name='results'),
-    # strona results
-    #path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    # ex /polls/5/vote/
-    #path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('<int:question_id>/vote/', views.ResultsView.vote, name='vote')
 ]

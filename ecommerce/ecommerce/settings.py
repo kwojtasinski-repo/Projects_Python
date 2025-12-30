f86dbf9d0d3b456b21074ead25fac6d9f91d494d
KW <k.wojtasinski@wp.pl>
2025-12-31 00:29:25 +0100

    [ecommerce] signup: add possibility to not send email

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -144,7 +144,23 @@ LOGGING = {
     },
 }
 
-FEATURE_SOCIAL_LOGIN = env_bool("FEATURE_SOCIAL_LOGIN", False)
+EMAILS_ENABLED = env_bool("EMAILS_ENABLED", False)
+
+if EMAILS_ENABLED:
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = os.getenv("EMAIL_HOST", "")
+    EMAIL_PORT = int(os.getenv("EMAIL_PORT", "587"))
+    EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "")
+    EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "")
+    EMAIL_USE_TLS = env_bool("EMAIL_USE_TLS", True)
+    DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "no-reply@example.com")
+    ACCOUNT_EMAIL_VERIFICATION = "mandatory"
+    ACCOUNT_EMAIL_REQUIRED = True
+else:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+    ACCOUNT_EMAIL_VERIFICATION = "none"
+    ACCOUNT_EMAIL_REQUIRED = False
+
 
 if ENVIRONMENT == 'production':
     DEBUG = False
